39fa5ca049113a92ef99b114a7189d31c5b0723b
Muhammed Shittu <devshittu@gmail.com>
2023-04-20 18:42:54 +0100

    accessed the xml file

---
+++ lxmlXMLFile.py
@@ -0,0 +1,20 @@
+from lxml import etree
+xml = open("food.xml","rb").read()
+#tree = etree.fromstring(xml)
+#tree = etree.parse(xml)
+tree = etree.XML(xml)
+
+print(tree)
+print(type(tree))
+
+#iter through all elements found in Tree
+for element in tree.iter():
+    print("%s - %s" % (element.tag, element.text))
+
+#iter through selected elements found in Tree
+for element in tree.iter('price','name'):
+    print(f"{element.tag} - {element.text}")
+
+#iter through description
+for element in tree.iter('description'):
+    print(f"{element.tag} - {element.text}")
--- parse.py
+++ parse.py
@@ -1,5 +1,4 @@
 from lxml import etree
-from io import BytesIO
 xml = open("food.xml", "rb").read()
 # xmlio = BytesIO(xml)
 tree = etree.parse(xml)
@@ -7,8 +6,8 @@ etree.tostring(tree)
 
 # tree = etree.XMLParser(xml)
 # tree = etree.parse(xml)
-# tree = etree.fromstring(xml)
-# tree = etree.parse(xml)
+tree = etree.fromstring(xml)
+tree = etree.parse(xml)
 
 # print(tree)
 # print(type(tree))
