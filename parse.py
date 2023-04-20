ad1bb84cd88b8c6b29ca2d5098bc8abb4c47ac86
Muhammed Shittu <devshittu@gmail.com>
2023-04-20 18:31:52 +0100

    Not working

--- parse.py
+++ parse.py
@@ -1,9 +1,17 @@
 from lxml import etree
+from io import BytesIO
 xml = open("food.xml", "rb").read()
+# xmlio = BytesIO(xml)
+tree = etree.parse(xml)
+etree.tostring(tree)
 
+# tree = etree.XMLParser(xml)
 # tree = etree.parse(xml)
 # tree = etree.fromstring(xml)
-tree = etree.parse(xml)
+# tree = etree.parse(xml)
 
-print(tree)
-print(type(tree))
+# print(tree)
+# print(type(tree))
+
+# for element in tree.iter():
+    # print(f"{element.tag} - {element.text}")
